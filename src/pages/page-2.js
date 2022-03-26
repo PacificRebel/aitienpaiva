539a248f85d27356bf7b8b04325ceb5c3603d313
Heli Sivunen <heli@mooncat.tv>
2022-03-26 17:28:05 +0000

    emoji added to page-2

---
+++ src/pages/page-2.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+const SecondPage = () => (
+  <Layout>
+    <br></br>
+    <br></br>
+    <h2 class="box">Ihanaa äitienpäivää maailman parhaimmalle äitille!!  <span role="img" aria-label="Hearts emoji">
+        💕
+      </span></h2>
+
+
+
+    <Link to="/" class="box">Takaisin kotisivulle</Link>
+  </Layout>
+)
+
+export default SecondPage
