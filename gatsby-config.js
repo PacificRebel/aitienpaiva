1657ec17f10039494cd7ec4dd76dfa5aef43ef08
Heli Sivunen <heli@mooncat.tv>
2022-03-26 16:17:34 +0000

    image and google font added, clickable text on front page

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"]
+  plugins: ["gatsby-plugin-gatsby-cloud", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }
+
+]
 };
---
+++ src/components/layout.js
@@ -0,0 +1,33 @@
+
+import React from "react"
+import PropTypes from "prop-types"
+import "@fontsource/inspiration"
+
+import "./layout.css"
+
+const Layout = ({ children }) => {
+
+  return (
+    <div>
+
+
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0 1.0875rem 1.45rem`,
+        }}
+      >
+        <main>{children}</main>
+
+      </div>
+
+    </div>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
